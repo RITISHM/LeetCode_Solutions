ef3471c4a822df72a8c0c79cd11edf9f61eb439d
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-01-08 22:19:41 +0530

    Time: 5 ms (22.66%) | Memory: 47.3 MB (51.08%) - LeetSync

--- Leetcode/268-missing-number/missing-number.java
+++ Leetcode/268-missing-number/missing-number.java
@@ -1,10 +1,11 @@
 import java.util.*;
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
+        HashSet <Integer> st=new HashSet<>();
         int i;
+        for (i=0;i<nums.length;i++)st.add(nums[i]);
         for (i=0;i<nums.length;i++){
-            if (nums[i]!=i)return i;
+            if (!st.contains(i))return i;
         }
         return i;
     }
