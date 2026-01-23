e03264a1a2f4ef34ef4aa073fb9ea0f6f4e63d9d
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-01-23 17:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (94.21%) - LeetSync

--- Leetcode/35-search-insert-position/search-insert-position.java
+++ Leetcode/35-search-insert-position/search-insert-position.java
@@ -1,14 +1,12 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int ans =0;
         int left =0;
         int right =nums.length-1;
         while (left <=right ){
             int mid =left +(right -left)/2;
-            if (nums[mid]<target)ans=mid+1;
             if (nums[mid]<target)left=mid+1;
             else right =mid -1;
         }
-        return ans ;
+        return left ;
     }
 }
