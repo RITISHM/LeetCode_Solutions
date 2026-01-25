7d4cd567196beba3f4176177960f8b1a006e3935
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-01-25 15:29:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (16.83%) - LeetSync

--- Leetcode/61-rotate-list/rotate-list.java
+++ Leetcode/61-rotate-list/rotate-list.java
@@ -9,20 +9,32 @@
  * }
  */
 class Solution {
+    
     public ListNode rotateRight(ListNode head, int k) {
-        if (head==null||head.next==null)return head;
-        ListNode fast=head;
-        while (fast.next!=null)fast=fast.next;
-        fast.next=head;
-        ListNode slow=fast;
-        fast=head;
-        for (int i=0;i<k;i++)fast=fast.next;
-        while (fast!=head){
-            fast=fast.next;
-            slow=slow.next;
+        if(head==null||head.next==null){
+            return head;
         }
-        head=slow.next;
-        slow.next=null;
+        int size=0;
+        ListNode tail=head;
+        ListNode start=head;
+    
+        while (tail.next!=null){
+            size++;
+            tail=tail.next;
+        }
+        size++;
+        k=k%size;
+        if(k==0){
+            return head;
+        }
+        tail.next=head;
+
+        
+        for(int i=1;i<size-k;i++){
+            start=start.next;
+        }
+        head=start.next;
+        start.next=null;
         return head;
     }
 }
