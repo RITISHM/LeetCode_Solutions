cebec4401203261b222a183a94fc1c0e5ade7371
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-05-09 01:28:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (84.86%) - LeetSync

--- leetcode/100-same-tree/same-tree.java
+++ leetcode/100-same-tree/same-tree.java
@@ -17,9 +17,8 @@ class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null&q==null)return true;
 
-        if (p==null||q==null)return false;
+        if (p==null||q==null||q.val!=p.val)return false;
 
-        if (q.val!=p.val) return false;
 
         return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
     }
