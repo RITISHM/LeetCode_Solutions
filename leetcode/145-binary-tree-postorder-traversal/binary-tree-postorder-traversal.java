5d18c3079d046ea0fc4903c3dd3e0c44e3efeacc
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-05-09 00:42:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (96.03%) - LeetSync

---
+++ leetcode/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static void transversal(TreeNode root, List<Integer> list){
+        if (root==null)return;
+        transversal(root.left,list);
+        transversal(root.right,list);
+        list.add(root.val);
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List <Integer> list= new LinkedList<>();
+        transversal(root,list);
+        return list;
+    }
+}
