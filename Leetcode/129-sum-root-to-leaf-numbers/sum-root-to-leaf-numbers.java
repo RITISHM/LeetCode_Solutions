8ad940c0259b30598a267bdc29d4350faac9a1c4
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-05-18 05:26:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (65.81%) - LeetSync

--- Leetcode/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ Leetcode/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -14,21 +14,19 @@
  * }
  */
 class Solution {
-    int sum=0;
-    void sumNumber(TreeNode root, int num){
-        if (root==null)return ;
+    int  sumNumber(TreeNode root, int num){
+        if (root==null)return 0;
+        num=num*10+root.val;
         if (root.left==null&& root.right==null){
-            sum+=(num*10+root.val);
-            return ;
+            return num;
         }
 
-        sumNumber(root.left,num*10+root.val);
-        sumNumber(root.right,num*10+root.val);
+       return  sumNumber(root.left,num)+sumNumber(root.right,num);
 
 
     }
     public int sumNumbers(TreeNode root) {
-        sumNumber(root,0);
-        return sum;
+        return sumNumber(root,0);
+        
     }
 }
