e36c2d756eca9836abbd87e9109408ab3db5ca89
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-05-18 06:45:29 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (13.43%) - LeetSync

--- Leetcode/113-path-sum-ii/path-sum-ii.java
+++ Leetcode/113-path-sum-ii/path-sum-ii.java
@@ -14,28 +14,23 @@
  * }
  */
 class Solution {
-    public static void dfs(TreeNode root , int sum, List<Integer> path, List<List<Integer>> res ){
-        if (root ==null)return ;
-
+    void dfs(TreeNode root,int target, List<Integer> path,List<List<Integer>> res ){
+        if (root==null)return ;
         path.add(root.val);
-
-        if (root.left==null && root.right==null && sum==root.val){
-            res.add(new ArrayList<>(path));
+        if (root.left==null&&root.right==null &&root.val==target){
+            res.add(new ArrayList(path));
         }
         else{
-            dfs(root.left,sum-root.val,path,res);
-            dfs(root.right,sum-root.val,path,res);
+        dfs(root.left,target-root.val,path,res);
+        dfs(root.right,target-root.val,path,res);
         }
+
         path.remove(path.size()-1);
-
     }
-
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> res =new ArrayList<>();
         List<Integer> path=new ArrayList<>();
         dfs(root,targetSum,path,res);
-
         return res;
-
     }
 }
