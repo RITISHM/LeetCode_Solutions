5da9d386bbed53b990990cd0260a4f8321044c23
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-06-01 04:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (16.81%) - LeetSync

--- leetcode/450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ leetcode/450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -16,22 +16,27 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root==null)return root;
-        if (root.val>key){
+
+        if (root.val>key){ //Transverse left subtree 
             root.left=deleteNode(root.left,key);
             
         } 
-        else if (root.val<key){
+        else if (root.val<key){ //Transverse right subtree
             root.right=deleteNode(root.right,key);
         }
 
+        //the current root is the key
         else{
+            //right subtree is not exist 
             if (root.right==null)return root.left;
+            //left subtree is not exist
             if (root.left==null)return root.right;
 
-
+            //bot right ans left exists 
             TreeNode right=root.right;
             TreeNode left =root.left;
 
+            //putting the right of the subtree to the right leaf of the left subtree
             TreeNode temp=left;
             while (temp.right!=null)temp=temp.right;
             temp.right=right;
